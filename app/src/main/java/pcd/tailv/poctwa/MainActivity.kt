c16a544b8e13885f0178b840d4f535c3a5f20ff5
Le Van Tai <vantai.le@live.com>
2021-12-24 11:27:08 +0700

    [Impl] PWA call native screen via Deep Links / Android Intent

--- app/src/main/java/pcd/tailv/poctwa/MainActivity.kt
+++ app/src/main/java/pcd/tailv/poctwa/MainActivity.kt
@@ -2,13 +2,12 @@ package pcd.tailv.poctwa
 
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.androidbrowserhelper.trusted.LauncherActivity
 import pcd.tailv.poctwa.databinding.ActivityMainBinding
-import com.google.androidbrowserhelper.trusted.TwaLauncher
-
-
 
 
 class MainActivity : AppCompatActivity() {
@@ -16,18 +15,22 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Toast.makeText(this, "MainActivity is running!", Toast.LENGTH_LONG).show()
+        Log.d("MainActivity", "onCreate")
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
         setup()
+
+
     }
 
     private fun setup() {
         binding.btnLaunchPwa.setOnClickListener {
-            val intent = Intent(this, LauncherActivity::class.java).apply {
-                action = Intent.ACTION_VIEW
+            val intent = Intent(this, CustomQueryStringLauncherActivity::class.java).apply {
+                data = Uri.parse(getString(R.string.launchUrl))
             }
             startActivity(intent)
         }
