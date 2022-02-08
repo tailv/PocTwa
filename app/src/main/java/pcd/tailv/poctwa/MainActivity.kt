d87471c8ad2e406ab80dc6299b506ac3324a4bf1
Lê Văn Tài <vantai.le@live.com>
2022-02-08 16:14:14 +0700

    Mod. Clean code for preparing report

--- app/src/main/java/pcd/tailv/poctwa/MainActivity.kt
+++ app/src/main/java/pcd/tailv/poctwa/MainActivity.kt
@@ -3,8 +3,6 @@ package pcd.tailv.poctwa
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.androidbrowserhelper.trusted.LauncherActivity
 import pcd.tailv.poctwa.databinding.ActivityMainBinding
@@ -15,8 +13,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        Toast.makeText(this, "MainActivity is running!", Toast.LENGTH_LONG).show()
-        Log.d("MainActivity", "onCreate")
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
@@ -29,7 +25,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun setup() {
         binding.btnLaunchPwa.setOnClickListener {
-            val intent = Intent(this, CustomQueryStringLauncherActivity::class.java).apply {
+            val intent = Intent(this, LauncherActivity::class.java).apply {
                 data = Uri.parse(getString(R.string.launchUrl))
             }
             startActivity(intent)
